75f06f560316e371271d7ee765c929bba3d6ba43
denni <denni@DESKTOP-09A66TK>
2023-03-27 06:15:41 +0300

    27.03.2023

--- Metotlar/Program.cs
+++ Metotlar/Program.cs
@@ -6,10 +6,52 @@ using System.Threading.Tasks;
 
 namespace Metotlar
 {
-    class Program
+    public class Metotlar
     {
         static void Main(string[] args)
         {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "amasya elması";
+
+            string[] meyveler = new string[] { };
+
+            Product urun1 = new Product();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Product urun2 = new Product();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            //type safe - tip güvenli
+
+            Product[] urunler = new Product[] { urun1, urun2 };
+            foreach (Product Urun in urunler)
+            {
+                Console.WriteLine(Urun.Adi);
+                Console.WriteLine(Urun.Fiyati);
+                Console.WriteLine(Urun.Aciklama);
+                Console.WriteLine("---------------------");
+            }
+
+            Console.WriteLine("-----------Metotlar------");
+
+            //istnace - örnek
+            //encapsulation
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "yeşil", 12 , 8);
+            sepetManager.Ekle2("elma", "yeşil elma", 12 , 4);
+            sepetManager.Ekle2("Karpuz", "Diyarbakır karpuzu", 12 , 21);
+
+            Console.ReadLine();
         }
+        
     }
 }
