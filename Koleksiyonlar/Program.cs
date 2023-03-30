63e7662195faa9cca45d74f50ebc1aacd5cf0344
denni <denni@DESKTOP-09A66TK>
2023-03-31 00:02:38 +0300

    31.03.2023

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] isimler = new string[] { "Engin", "Murat", "Kerem", "Halil" };
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            Console.WriteLine(isimler[2]);
+            Console.WriteLine(isimler[3]);
+            //       new Yapmak heap üzerinde yeni değer atamak demektir. bu sebeple isimler dizisinin 5. elemanına "ilker" atamış olduk ama ilk 4 elmanı atamadığımı için boş olacaktır.
+            isimler = new string[5];
+            isimler[4] = "İlker";
+            Console.WriteLine(isimler[4]);
+            Console.WriteLine(isimler[0]);
+
+            List<string> isimler2 = new List<string> { "Engin", "Murat", "Kerem", "Halil" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("İlker");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
--- Koleksiyonlar/Properties/AssemblyInfo.cs
+++ Koleksiyonlar/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WindowsFormsApp1")]
+[assembly: AssemblyTitle("Koleksiyonlar")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WindowsFormsApp1")]
+[assembly: AssemblyProduct("Koleksiyonlar")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("21d0c229-882e-460a-a873-a0b52f4b3834")]
+[assembly: Guid("d3750f89-3358-4cc5-a9eb-2476cf9e0aaa")]
 
 // Version information for an assembly consists of the following four values:
 //
